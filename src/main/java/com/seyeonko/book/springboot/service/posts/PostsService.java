2c66b87c5ab774f2db2f2b8435a8ca5727da6b95
Seyeon Ko <seyeonko97@gmail.com>
2021-03-19 17:51:55 +0900

    글 삭제 기능 추가

--- src/main/java/com/seyeonko/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/seyeonko/book/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.seyeonko.book.springboot.service.posts;
 
 import com.seyeonko.book.springboot.domain.posts.Posts;
 import com.seyeonko.book.springboot.domain.posts.PostsRepository;
+import com.seyeonko.book.springboot.web.dto.PostsListResponseDto;
 import com.seyeonko.book.springboot.web.dto.PostsResponseDto;
 import com.seyeonko.book.springboot.web.dto.PostsSaveRequestDto;
 import com.seyeonko.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,9 @@ import javafx.geometry.Pos;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,7 +35,20 @@ public class PostsService {
     public PostsResponseDto findById (Long id){
 
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id =" + id ));
-
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts);
     }
 }
