32efa7eaacca67459407ab60e64198f0cbdf32ba
Seyeon Ko <seyeonko97@gmail.com>
2021-03-21 17:10:43 +0900

    구글 로그인 기능 추가 글 등록 시 권한 없음 이슈 해결 필요

--- src/main/java/com/seyeonko/book/springboot/web/IndexController.java
+++ src/main/java/com/seyeonko/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.seyeonko.book.springboot.web;
 
+import com.seyeonko.book.springboot.config.auth.LoginUser;
+import com.seyeonko.book.springboot.config.auth.dto.SessionUser;
 import com.seyeonko.book.springboot.service.posts.PostsService;
 import com.seyeonko.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +17,11 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -29,7 +34,7 @@ public class IndexController {
     public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
+
         return "posts-update";
     }
-
 }
